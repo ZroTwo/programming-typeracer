95a8b5731ae8c62d674cc2bdf4790a2cdb8da4e3
Gino-Landstede <gkamperm9970@student.landstede.nl>
2022-06-16 09:54:03 +0200

    last push to the branch

--- resources/views/typeracer/show.blade.php
+++ resources/views/typeracer/show.blade.php
@@ -1,9 +1,10 @@
 <x-guest-layout>
     <div>
         <div class="flex justify-center py-3">
-            <a href="" class="   px-2 py-1 text-white border border-slate-800 bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 hover:bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500 rounded">Start</a>
+            <a href="" class=" mr-96  px-2 py-1 text-white border border-slate-800 bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 hover:bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500 rounded">Start</a>
+            <span class=" text-white pl-20 pr-10">WPM</span>
+            <span class="text-white">Accuracy %</span>
         </div>
-
 
         <div class="flex mr-auto ml-auto w-3/6 h-96 bg-slate-700 border border-gray-400 rounded-lg">
 
